6771fe166ce5f19a73bcaffde81dba1aeb81f49f
Vikas Sen <Vikas.Sen@Virtustream.com>
2019-06-15 11:20:03 +0530

    Promise all and race

---
+++ promise/promiseAll_1.js
@@ -0,0 +1,26 @@
+// Promise.all lets you know when either all input promises have fulfilled or when one of them rejects.
+
+(
+    async () => {
+        const fulfilledPromise1 = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('done 1');
+                resolve();
+            }, 300);
+        });
+        const fulfilledPromise2 = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('done 2');
+                resolve();
+            }, 1000);
+        });
+        const fulfilledPromise3 = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('done 3');
+                resolve();
+            }, 800);
+        });
+        await Promise.all([fulfilledPromise1, fulfilledPromise2, fulfilledPromise3]);
+        console.log('Once all the promises resolved, this log will be printed.');
+    }
+)();
---
+++ promise/promiseAll_2.js
@@ -0,0 +1,31 @@
+// Promise.all lets you know when either all input promises have fulfilled or when one of them rejects.
+
+(
+    async () => {
+        try {
+            const fulfilledPromise1 = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log('done 1');
+                    resolve();
+                }, 300);
+            });
+            const fulfilledPromise2 = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log('done 2');
+                    resolve();
+                }, 1000);
+            });
+            const fulfilledPromise3 = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log('fail 3');
+                    reject();
+                }, 800);
+            });
+            await Promise.all([fulfilledPromise1, fulfilledPromise2, fulfilledPromise3]);
+            console.log('Case1: all the promise logs should be printed.');
+        }
+        catch {
+            console.log('Promise rejected... This log should be printed once any promise is failed.');
+        }
+    }
+)();
